cf13fc81437a57159a6abd96f7cd65b8b1bbf7f6
davidsaravia1 <saravia20cristofer@gmail.com>
2024-06-16 18:23:14 -0500

    Add files via upload

---
+++ hola.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNIDAD EDUCATIVA FISCAL "SIMÓN BOLÍVAR"</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Change Background Color</title>
+    <?php
+    // Define el color de fondo aquí, si es necesario
+    $backgroundColor = "#f0f0f0"; // Puedes cambiar este valor o hacerlo dinámico
+    echo "<style>body { background-color: $backgroundColor; }</style>";
+    ?>
+</head>
+<body>    
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container">
+        <a href="#" class="navbar-brand"><span class="text-primary">𝓤𝓝𝓘𝓓𝓐𝓓 𝓔𝓓𝓤𝓒𝓐𝓣𝓘𝓥𝓐 𝓕𝓘𝓢𝓒𝓐𝓛 ❞𝓢𝓘𝓜𝓞́𝓝 𝓑𝓞𝓛𝓘́𝓥𝓐𝓡❞</span></a>
+        <a href="destino.html" class="boton-redireccion">Más Información</a>
+        <div class="collapse navbar-collapse" id="navbar-start">
+            <ul class="navbar-nav ms-auto mb-6 mb-lg-3">
+            </ul>
+        </div>
+    </div>
+</nav>
+<p>.</p>
+<p>.</p>
+    
+<div id="carouselE1" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselE1" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselE1" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselE1" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="img/fondocole3.jpeg" class="d-block w-100" alt="">
+            <div class="carousel-caption"></div>
+        </div>
+        <div class="carousel-item">
+            <img src="img/fondocole2.jpeg" class="d-block w-100" alt="">
+            <div class="carousel-caption"></div>
+        </div>
+        <div class="carousel-item">
+            <img src="img/fondocole1.jpg.jpeg" class="d-block w-100" alt="">
+            <div class="carousel-caption"></div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselE1" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselE1" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<section class="services">
+    <div class="container">
+        <div class="card-body">
+            <div class="col-12 col-md-100 col-lg-100 mb-3">
+                <a class="card text-white text-center bg-car14 pb-2" style="text-decoration: none;">
+                    <div class="card-body">
+                        <p>.</p>
+                        <h3 class="card-title">ESPECIALIDADES QUE OFRECE LA INSTITUCIÓN</h3>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <p>.</p>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-6 col-lg-4 mb-3">
+                    <a href="area_informatica.html" class="card text-white text-center bg-car4 pb-2" style="text-decoration: none;">
+                        <div class="card-body">
+                            <h3 class="card-title">ÁREA DE INFORMÁTICA</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4 mb-3">
+                    <a href="area_contabilidad.html" class="card text-white text-center bg-car5 pb-2" style="text-decoration: none;">
+                        <div class="card-body">
+                            <h3 class="card-title">ÁREA DE CONTABILIDAD</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4 mb-3">
+                    <a href="area_ciencias.html" class="card text-white text-center bg-car6 pb-2" style="text-decoration: none;">
+                        <div class="card-body">
+                            <p>.</p>
+                            <h3 class="card-title">ÁREA DE CIENCIAS</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section-icons">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-4 box-icons">
+                <div class="d-flex align-items-center">
+                    <i class="bi bi-alarm-fill"></i>
+                    <div class="ms-4">
+                        <h5>HORARIO DE INGRESO Y SALIDA DEL PLANTEL</h5>
+                        <p>Horario Matutina  6:45am - 13:15pm</p>
+                        <p>Horario Vespertina  12:45pm - 6:00pm</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-4 box-icons">
+                <i class="bi bi-align-items-center"></i>
+                <div class="ms-4">
+                    <h5>PROGRAMAS AVANZADOS</h5>
+                    <p>Los mejores programas en la actualidad</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-4 box-icons">
+                <i class="bi bi-align-items-center"></i>
+                <div class="ms-4">
+                    <h5>LA MEJOR EDUCACIÓN</h5>
+                    <p>Los Docentes más apropiados para tu educación</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ hola1.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ECUAVOLEY</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="img/voley.webp" class="d-block w-100" alt="Imagen de ecuavoley">
+            <div class="carousel-caption"></div>
+        </div>
+    </div>
+    <section class="product">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-header text-center pb-4">
+                        <h2>SELECCIÓN DE ECUAVOLEY</h2>
+                        <p>La Selección de Ecuavoley es otro de los grupos deportivos destacados de esta institución. 
+                        En abril de 2023 se consagraron campeones del Campeonato Intercolegial de Ecuavoley de la 
+                        Federación Deportiva Estudiantil.</p>
+                        <p>En el Colegio Simón Bolívar, el ecuavoley es una actividad deportiva destacada que fomenta 
+                        la participación y el desarrollo físico de los estudiantes. Este deporte, 
+                        adaptado de las tradiciones ecuatorianas, se juega con equipos de tres jugadores cada uno 
+                        y utiliza una cancha similar a la de voleibol pero con una red más alta. Las reglas 
+                        particulares del ecuavoley, que permiten un mayor contacto con la pelota y un estilo de 
+                        juego más dinámico, hacen que sea un deporte accesible y emocionante para los estudiantes.</p>
+                        <p>El ecuavoley no solo promueve la actividad física, sino que también inculca valores esenciales
+                        como el trabajo en equipo, la disciplina y el respeto por los demás. A través de torneos 
+                        internos y actividades extracurriculares, los alumnos de la Unidad Educativa Fiscal "Simón Bolívar" tienen la 
+                        oportunidad de mejorar sus habilidades técnicas y fortalecer su condición física.</p>
+                        <p>Esta práctica deportiva se integra en el programa educativo del colegio, apoyando el bienestar 
+                        integral de los estudiantes y proporcionando un espacio para la recreación y la socialización.
+                        Así, el ecuavoley en la Institución se convierte en una herramienta educativa que 
+                        contribuye al desarrollo personal y comunitario de sus alumnos.</p>
+                    </div>
+                </div>
+            </div> 
+            <div class="row">
+                <div class="col-12">
+                    <div class="card bg-light shadow-sm border-0 px-4 py-3 mb-4">
+                        <div class="text-center">
+                            <img src="img/voley1.jpg" alt="Imagen del equipo de ecuavoley">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-header text-center pb-5">
+                        <h2>Logros y Reconocimientos</h2>
+                        <p>El 5 de abril de 2023 se desarrolló la final del Campeonato Intercolegial de Ecuavoley en el Coliseo Los Quitus de Concentración Deportiva de Pichincha, donde la Unidad Educativa Fiscal Simón Bolívar se llevó el triunfo sobre la Unidad Educativa Primicias de la Cultura de Quito.</p>
+                        <p>Esta es la primera vez que Simón Bolívar participa en la competición y para Medardo Tapia, entrenador del equipo, este triunfo es una ‘‘alegría inmensa’’. Así mismo, han sido invitados para disputar un torneo provincial, por lo cual continuará trabajando en conjunto con los estudiantes de la institución.</p>
+                        <p>De este modo se cumplió otra jornada donde la Concentración Deportiva de Pichincha ha puesto al servicio de la Federación Deportiva Estudiantil sus escenarios para el desarrollo de competencias y fortalecimiento del deporte en la provincia.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <a href="index.2.html" class="boton-redireccion">MENU PRINCIPAL </a>
+    <a href="destino.html" class="boton-redireccion">ATRAS </a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
